45b840b27b698f44acaadc0b71724604654181ce
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2022-09-24 23:10:21 +0000

    Punto 2

--- Riva/src/app/app.module.ts
+++ Riva/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PizzaComponent } from './pizza/pizza.component';
+import { PastaComponent } from './pasta/pasta.component';
+import { MandolinoComponent } from './mandolino/mandolino.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaComponent
+    PizzaComponent,
+    PastaComponent,
+    MandolinoComponent
   ],
   imports: [
     BrowserModule
---
+++ Riva/src/app/mandolino/mandolino.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mandolino',
+  templateUrl: './mandolino.component.html',
+  styleUrls: ['./mandolino.component.css']
+})
+export class MandolinoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
