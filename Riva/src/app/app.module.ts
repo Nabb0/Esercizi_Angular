aa9e37c7157ff0085e6d3d56088af4908fef67a5
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2022-09-24 22:30:31 +0000

    Punto 2

--- Riva/src/app/app.module.ts
+++ Riva/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PizzaComponent } from './pizza/pizza.component';
+import { PastaComponent } from './pasta/pasta.component';
+import { MandolinoComponent } from './mandolino/mandolino.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaComponent,
+    PastaComponent,
+    MandolinoComponent
   ],
   imports: [
     BrowserModule
