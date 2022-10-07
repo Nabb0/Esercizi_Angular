a99c91051b62fc3d6bdcd94addd9f466330eda96
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2022-10-07 08:35:55 +0000

    es 1

---
+++ Preparazione_alla_verifica_su_vettori/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Preparazione_alla_verifica_su_vettori';
+}
